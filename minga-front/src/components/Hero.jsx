2fd3d90dad0684c947ce423d72acf125e34db4d3
Gustavo alexander Vasquez More <123557057+Gustavo-Alexander-Vasquez-More@users.noreply.github.com>
2023-06-28 02:22:02 -0300

    minga-10

---
+++ minga-front/src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import Button from "./Button";
+export default function Hero() {
+  return (
+    <div className="w-full h-[85vh] flex flex-col items-center justify-center md:flex md:flex-col md:h-[55vh] gap-8">
+    <div className="w-[90%] h-[12vh] md:h-[4rem] lg:flex lg:justify-center" id="TITULO" >
+      <h1 className="text-[#FFF] text-[2rem] text-center font-roboto font-bold sm:text-[2.3rem] lg:w-[70%] lg:flex lg:justify-center lg:items-center">Your favorite comic book store<p className="hidden lg:block lg:w-[2rem] lg:h-[2.2rem] lg:text-[1.5rem]">✨</p></h1>
+    </div>
+    <div className="md:h-[2rem] md:w-[70%] md:text-center md:text-[1.2rem] md:text-[#FFF] hidden lg:w-[40%] lg:h-[4rem] lg:block">Explore our catalog to live the adventure of your life </div>
+<Button title='Let´s go!'/>
+    
+    <div id="texto-mobile" className="w-[90%] text-center h-[13vh] flex items-center md:w-[60%]  ">
+      <p className="  text-[#FFF] text-[0.9rem] sm:text-[1.1rem] lg:hidden " >From classics to novelties, we have everything you need to immerse yourself in your favorite universes. Explore our catalog and live the adventure of your life.</p>
+    </div>
+    <button id="buton-mobile" className="md:hidden bg-[#5b34c5] w-[80%] h-[6.5vh] flex justify-center items-center text-[#FFF] text-[1.5rem] rounded-[50px] mb-[4rem] sm:w-[50%]">Started</button>
+  </div>
+  );
+}
--- minga-front/src/pages/Index.jsx
+++ minga-front/src/pages/Index.jsx
@@ -1,31 +1,9 @@
+import Carrousel from "../components/Carrousel";
 export default function index() {
   return (
     <div>
     <main className="hidden lg:block lg:h-[35vh] lg:w-[100%] lg:flex justify-center items-center">
-      <div  id="bg-container-main" className="hidden md:block bg-[#5b34c5] md:w-[90%] lg-w-[90%] xl-w-[95%] md:h-[24vh] md:flex rounded-[15px] " >
-        <div id="seccion-izquierda" className="w-[50%] h-[24vh] md:flex md:items-center justify-around">
-        <div className="w-[30%] h-[24vh] flex justify-start items-center px-[0.7rem]">
-        <svg className="lg:bg-[#FFF] lg:h-[1.5rem] lg:w-[1.5rem] rounded-[15px] p-[5px] lg:ml-[1rem]"  width="11" height="10" viewBox="0 0 11 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path id="Vector (Stroke)" fill-rule="evenodd" clip-rule="evenodd" d="M10.25 5.25C10.25 5.00147 10.0465 4.8 9.79545 4.8L1.80192 4.8L5.11687 1.5182C5.29438 1.34246 5.29438 1.05754 5.11687 0.881802C4.93936 0.706066 4.65155 0.706066 4.47404 0.881802L0.383134 4.9318C0.205624 5.10754 0.205624 5.39246 0.383134 5.5682L4.47404 9.6182C4.65155 9.79393 4.93936 9.79393 5.11687 9.6182C5.29438 9.44246 5.29438 9.15754 5.11687 8.9818L1.80192 5.7L9.79545 5.7C10.0465 5.7 10.25 5.49853 10.25 5.25Z" fill="#333333"/>
-</svg>  
-        </div>
-        
-<img className="lg:h-[12rem] lg:w-[10rem] lg:mb-[3rem] lg:mr-[4.5rem] xl:h-[13rem] xl:w-[10rem]" src="/izuko.png" alt="" />
-<img className="md:h-[11rem] lg:w-[7rem] md:mb-[3rem] lg:mr-[4rem] xl:w-[9rem] xl:h-[11rem]" src="/banner-home.png" alt="" />
-        </div>
-        <div id="secccion-derecha" className=" w-[50%] h-[24vh] md:flex md:justify-start md:items-center">
-        <div id="texto-sec-derecha" className="md:flex md:flex-col text-[#FFF] lg:w-[65%]">
-          <h2>Shonen</h2>
-          <p className="text-[0.8rem]">Is the manga that is aimed at adolescent boys. They are series with large amounts of action, in which humorous situations often occur. The camaraderie between members of a collective or a combat team stands out.</p>
-        </div>
-        <div id="sec-flecha-derecha" className="w-[35%] h-[24vh] flex justify-end items-center lg:px-[2rem]">
-        <svg className="md:bg-[#FFF] md:h-[1.5rem] md:w-[1.5rem] rounded-[15px] p-[5px]" width="11" height="10" viewBox="0 0 11 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path id="Vector (Stroke)" fill-rule="evenodd" clip-rule="evenodd" d="M0.25 5.25C0.25 5.00147 0.453507 4.8 0.704545 4.8L8.69808 4.8L5.38313 1.5182C5.20562 1.34246 5.20562 1.05754 5.38313 0.881802C5.56064 0.706066 5.84845 0.706066 6.02596 0.881802L10.1169 4.9318C10.2944 5.10754 10.2944 5.39246 10.1169 5.5682L6.02596 9.6182C5.84845 9.79393 5.56064 9.79393 5.38313 9.6182C5.20562 9.44246 5.20562 9.15754 5.38313 8.9818L8.69808 5.7L0.704545 5.7C0.453507 5.7 0.25 5.49853 0.25 5.25Z" fill="#333333"/>
-</svg>
-
-        </div>
-        </div>
-      </div>
+      <Carrousel/>
     </main>  
     </div>
   );
